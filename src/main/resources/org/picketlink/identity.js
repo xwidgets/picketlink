33b73b8de2c192e66a7fc6a2c4efcdc78488f636
sbryzak <sbryzak@gmail.com>
2014-05-05 12:26:24 +1000

    minor

--- src/main/resources/org/picketlink/identity.js
+++ src/main/resources/org/picketlink/identity.js
@@ -5,17 +5,32 @@ org.picketlink.Identity = xw.NonVisual.extend({
     this._super(false);
     this.registerProperty("binding", {default: "identity"});
     this.registerProperty("url");
+    this.loggedIn = false;
+    this.account = null;
   },
   open: function() {
-  
+    xw.EL.registerResolver(this);  
   },
-  canResolve: function(expr) {
+  loginCallback: function() {
+    this.loggedIn = pl.loggedIn;
+    this.account = pl.account;
+    xw.EL.notify(this.binding.value);
+    xw.Event.fire("org.picketlink.identity.loggedIn");
+  },
+  login: function(user, pass) {
+    var that = this;
+    var cb = function() {
+      that.loginCallback();
+    }
+    pl.login(user, pass, cb);
+  },
+  canResolve: function(expr) {    
     return expr == this.binding.value;
   },
   resolve: function(expr) {
-    if (expr == this.binding.value) {
-
-    }  
+    if (this.canResolve(expr)) {
+      return this;
+    } 
   },
   toString: function() {
     return "org.picketlink.Identity";
